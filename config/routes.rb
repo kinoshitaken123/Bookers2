4fec1d0a5ab47ccf32bb4197de3584d54252800f
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-09 08:42:36 +0000

    [Addページ毎のURL作成]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "homes#top"
-  get '/home/about'
+  get '/home/about'=> "homes#about"
 
   resources :users, only: [:index, :show, :edit, :update]
 
